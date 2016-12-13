f893c43f58a441ddf210967a39a5a37ef92214e9
Boris Mikhaylov <kaguxmail@gmail.com>
2016-12-13 13:28:23 +0100

    updated specs

--- lib/ex_dns_client.ex
+++ lib/ex_dns_client.ex
@@ -4,31 +4,36 @@ defmodule ExDnsClient do
   Thin wrapper around erlang's `inet_res` library 
   """
 
+  @type ip4_address :: {0..255, 0..255, 0..255, 0..255}
+  @type ip6_address :: {0..65535, 0..65535, 0..65535, 0..65535, 0..65535, 0..65535, 0..65535, 0..65535}
+  @type ip_address :: ip4_address | ip6_address
+  @type nameserver :: {ip_address, {:port, 1..65535}}
+  @type dns_record_class :: [:in | :chaos | :hs | :any]
+  @type dns_record_type :: [ :a | :aaaa | :cname | :gid | :hinfo | :ns | :mb | :md |
+                         :mg | :mf | :minfo | :mx | :naptr | :null | :ptr | :soa |
+                         :spf | :srv | :txt | :uid | :uinfo | :unspec | :wks ]
+
   @doc """
   Resolves the DNS data for the record of the specified type and class for the specified name.
   On success, filters out the answer records with the correct Class and Type, and returns a list of their data fields.
   So, a lookup for type any gives an empty answer, as the answer records have specific types that are not any.
   An empty answer or a failed lookup returns an empty list.
   """
-  # @type nameserver 
-  @type record_class :: [:in | :chaos | :hs | :any]
-  @type record_type :: [ :a | :aaaa | :cname | :gid | :hinfo | :ns | :mb | :md |
-                         :mg | :mf | :minfo | :mx | :naptr | :null | :ptr | :soa |
-                         :spf | :srv | :txt | :uid | :uinfo | :unspec | :wks ]
   @type lookup_opts :: [
-    {:class, record_class} |
-    {:type, record_type} |
-    {:alt_nameservers, [:inet_res.nameserver, ...]} |
+    {:class, dns_record_class} |
+    {:type, dns_record_type} |
+    {:alt_nameservers, [nameserver, ...]} |
     {:edns, 0 | false} |
     {:inet6, boolean} |
-    {:nameservers, [:inet_res.nameserver, ...]} |
+    {:nameservers, [nameserver, ...]} |
     {:recurse, boolean} |
     {:retry, integer} |
     {:timeout, integer} |
     {:udp_payload_size, integer} |
     {:usevc, boolean}
   ]
-  @spec lookup(name :: String.t, opts :: lookup_opts) :: []
+  @type dns_data :: [String.t] | [ip4_address] | [ip6_address]
+  @spec lookup(name :: String.t, opts :: lookup_opts) :: dns_data
   def lookup(name, opts \\ []) do
     class = opts |> Keyword.get(:class, :in)
     type = opts |> Keyword.get(:type, :a)
